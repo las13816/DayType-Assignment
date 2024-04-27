0a6214ac8da981f63769b3a05ca031956b80eb54
las13816 <las13816@email.vccs.edu>
2024-04-27 14:13:38 -0400

    dayType created on master

---
+++ dayType.h
@@ -0,0 +1,27 @@
+#ifndef dayType_H
+#define dayType_H
+
+#include <string>
+
+using namespace std;
+
+class dayType
+{
+public:
+	static string weekDays[7];
+	void print() const;
+	string nextDay() const;
+	string prevDay() const;
+	void addDay(int nDays);
+
+	void setDay(string d);
+	string getDay() const;
+
+	dayType();
+	dayType(string d);
+
+private:
+	string weekDay;
+};
+
+#endif
